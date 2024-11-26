53d11f6f80c012184854ce9b66ceb5fc32bc4d02
kaoutar <elazamikaoutar@gmail.com>
2024-11-26 10:35:55 +0100

    parent child done

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,16 +1,25 @@
-import { Component , Input, OnInit } from '@angular/core';
+import { Component , EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Course } from './course';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-course',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './course.component.html',
   styleUrl: './course.component.css'
 })
 export class CourseComponent implements OnInit{
-  @Input() content! : Course;
+  @Input() content! : Course; 
+  @Output() newNb = new EventEmitter<number>();
+
+  lastNb=0;
+  
   constructor(){}
   ngOnInit(){
   }
-
+  updateNb(){
+    let nb = this.content.nb_student - this.lastNb;
+    this.lastNb = this.content.nb_student;
+    this.newNb.emit(nb);
+  }
 }
--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Course } from '../course/course';
+import { CourseComponent } from "../course/course.component";
 
 @Component({
   selector: 'app-courses',
-  imports: [FormsModule],
+  imports: [FormsModule, CourseComponent],
   standalone:true,
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'  
 })
 export class CoursesComponent implements OnInit {
+
   title!: "Courses Lists !";
   UE : Course[] = [{title:"c1",nb_student:1},{title:"c2",nb_student:2}]
-
+  nb_students:number=0;
   getTitle(){return this.title;}
+
+  getNbStudent(){
+    this.nb_students = 0;
+    for(let ue of this.UE) this.nb_students += ue.nb_student;
+  }
+  onNewNb(delta: number) {
+    this.nb_students += delta;
+  }
 
   constructor(){}
   ngOnInit() {
